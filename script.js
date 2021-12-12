5fb67272f3cbb7482f55bb12738c2244f5e9696a
Jeff Kim <jeffersontaekim@gmail.com>
2021-12-11 19:45:39 -0800

    Add function for who wins

--- script.js
+++ script.js
@@ -5,15 +5,53 @@ function computerPlay() {
 
     switch(num) {
         case 0:
-            choice = "Rock";
+            choice = "rock";
             break;
         case 1:
-            choice = "Paper";
+            choice = "paper";
             break;
         case 2:
-            choice = "Scissors";
+            choice = "scissors";
             break;
         
     }
     return choice;
 }
+
+//Takes user input and computer input and decides outcome
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
+        return "Invalid choice";
+    }
+    if (playerSelection == computerPlay) {
+        return "Tie!";
+    }
+    else if (playerSelection == "rock") {
+        if (computerPlay == "paper") {
+            return "Computer picked paper, you lose!";
+        }
+        else {
+            return "Computer picked scissors, you win!";
+        }
+    }
+    else if (playerSelection == "paper") {
+        if (computerPlay == "rock") {
+            return "Computer picked rock, you win!";
+        }
+        else {
+            return "Computer picked scissors, you lose!";
+        }
+    }
+    else {
+        if (computerPlay == "rock") {
+            return "Computer picked rock, you lose!";
+        }
+        else {
+            return "Computer picked paper, you win!";
+        }
+    }
+}
+
+const computerSelection = computerPlay();
+const playerSelection = "rock";
