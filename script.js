56407d3351f4e7b7d274f0234ba1653474e2e9d7
Jeff Kim <jeffersontaekim@gmail.com>
2022-01-04 21:02:06 -0800

    Create IU interface

--- script.js
+++ script.js
@@ -56,13 +56,43 @@ function playRound(playerSelection, computerSelection) {
 
 
 
+let playerWins = 0;
+let computerWins = 0;
+
+document.getElementById('playerWins').innerHTML = `Player: ${playerWins}`;
+document.getElementById('computerWins').innerHTML = `Computer: ${computerWins}`;
+
 
 
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(playRound(button.id, computerPlay()));
+        let result = playRound(button.id, computerPlay());
+        document.getElementById('result').innerHTML = result;
+        
+
+        
+        let win = /win/;
+        let lose = /lose/;
+        if (win.test(result)) {
+            playerWins++;
+            document.getElementById('playerWins').innerHTML = `Player: ${playerWins}`;
+            if (playerWins == 5) {
+
+            }
+        }  
+        else if (lose.test(result)) {
+            computerWins++;
+            document.getElementById('computerWins').innerHTML = `Computer: ${computerWins}`;
+        }
+
+        if (playerWins == 5) {
+            document.getElementById('final').innerHTML = 'Player wins!';
+        }
+        else if (computerWins == 5) {
+            document.getElementById('final').innerHTML = 'Computer wins!';
+        }
        
     });
 });
